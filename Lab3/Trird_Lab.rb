16129f2584503325ac7a568c9fa3a39291be58c1
OBALDEET <kep4ik2001@gmail.com>
2021-05-17 01:30:40 +0300

    )

--- Lab3/Trird_Lab.rb
+++ Lab3/Trird_Lab.rb
@@ -1,9 +1,9 @@
 def read_file (age = gets.to_i)
-  file = File.open("input.txt")
+  file = File.open("Lab3/input.txt")
   file_data = file.readlines.map{|line| line.chomp}
   file.close
 
-  res_file = File.open("result.txt", "w")
+  res_file = File.open("Lab3/result.txt", "w")
   file_data.each do |data|
     if data.include?(age.to_s)
       res_file.write(data + "\n")
@@ -11,24 +11,24 @@ def read_file (age = gets.to_i)
   end
   res_file.close
 
-  result_file = File.open("result.txt")
+  result_file = File.open("Lab3/result.txt")
   res_file_data = result_file.readlines.map{|line| line.chomp}
   print res_file_data
   puts ""
 end
 
 def open_file
-  if File.exist?("balance.txt")
-    file = File.open("balance.txt")
+  if File.exist?("Lab3/balance.txt")
+    file = File.open("Lab3/balance.txt")
   else
-    file = File.open("balance.txt", "w+")
+    file = File.open("Lab3/balance.txt", "w+")
     file.puts("100")
     file.close
   end
 end
 
 def deposit
-  file = File.open("balance.txt")
+  file = File.open("Lab3/balance.txt")
   file_data = file.readlines.map{|line| line.chomp}
   current_balance = file_data[0]
   file.close
@@ -37,14 +37,14 @@ def deposit
   if sum.to_i < 0
     puts "Невозможно пополнить счет на отрицательную сумму"
   end
-  file_write = File.open("balance.txt", "w")
+  file_write = File.open("Lab3/balance.txt", "w")
   new_balance = sum.to_i + current_balance.to_i
   file_write.write(new_balance)
   file_write.close
 end
 
 def withdraw
-  file = File.open("balance.txt")
+  file = File.open("Lab3/balance.txt")
   file_data = file.readlines.map{|line| line.chomp}
   current_balance = file_data[0]
   file.close
@@ -56,14 +56,14 @@ def withdraw
     puts "На счету недостаточно средств"
     return -1
   end
-  file_write = File.open("balance.txt", "w")
+  file_write = File.open("Lab3/balance.txt", "w")
   new_balance = current_balance.to_i - sum.to_i
   file_write.write(new_balance)
   file_write.close
 end
 
 def balance
-  file = File.open("balance.txt")
+  file = File.open("Lab3/balance.txt")
   file_data = file.readlines.map{|line| line.chomp}
   puts file_data[0]
   file.close
@@ -106,4 +106,3 @@ def main
 end
 
 main
-#where you find this?
